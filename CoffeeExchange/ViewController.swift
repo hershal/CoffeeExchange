840e83bf80fd9e016e30a4c5486fc09d5543f5db
Hershal Bhave <hershal.bhave@gmail.com>
2016-04-22 17:18:19 -0500

    Initial Commit

---
+++ CoffeeExchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoffeeExchange
+//
+//  Created by Hershal Bhave on 2016-04-22.
+//  Copyright © 2016 Hershal Bhave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
