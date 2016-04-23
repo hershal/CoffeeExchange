d513505ac885bb33cd99405a70c237740239aa0f
Hershal Bhave <hershal.bhave@gmail.com>
2016-04-23 16:05:44 -0500

    animates with random angle

--- CEEntryDetailViewController.swift
+++ CEEntryDetailViewController.swift
@@ -20,20 +20,21 @@ class CEEntryDetailViewController: UIViewController, UITableViewDelegate {
         refreshView()
 
         // gives a random float between 0 and 1
-        let randomFloat = CGFloat(Float(arc4random()) / Float(UINT32_MAX))
-        let xSpawn = (detailBackgroundView.frame.width-100) * randomFloat
-        let ySpawn = detailBackgroundView.frame.origin.y
-        let square = UIView(frame: CGRect(x: xSpawn, y: ySpawn, width: 100, height: 100))
-        square.backgroundColor = UIColor.brownColor()
-        detailBackgroundView.addSubview(square)
-        gravity.addItem(square)
+        let randomFloat = CGFloat.random()
+        let xSpawn = (detailBackgroundView.frame.width - CEEntryDynamicItem.size.width) * randomFloat
+        let ySpawn = CGFloat(100) // detailBackgroundView.frame.origin.y
+        let origin = CGPoint(x: xSpawn, y: ySpawn)
+        let dynamicItem = CEEntryDynamicItem(origin: origin)
+        detailBackgroundView.addSubview(dynamicItem)
+        gravity.addItem(dynamicItem)
+        boundsCollision.addItem(dynamicItem)
     }
 
     var entry: CEEntry!
     var delegate: CEEntryDetailDelegate?
     var animator: UIDynamicAnimator!
     var gravity: UIGravityBehavior!
-    var ground: UICollisionBehavior!
+    var boundsCollision: UICollisionBehavior!
 
     func refreshView() {
         if self.viewIfLoaded != nil {
@@ -62,8 +63,10 @@ class CEEntryDetailViewController: UIViewController, UITableViewDelegate {
 
         animator = UIDynamicAnimator(referenceView: detailBackgroundView)
         gravity = UIGravityBehavior()
-        ground = UICollisionBehavior()
-//        gravity.addChildBehavior(ground)
+        boundsCollision = UICollisionBehavior()
+        boundsCollision.translatesReferenceBoundsIntoBoundary = true
+
+        animator.addBehavior(boundsCollision)
         animator.addBehavior(gravity)
     }
 }
@@ -79,12 +82,28 @@ class CEEntryDetailBackgroundView: UIView {
 }
 
 class CEEntryDynamicItem: UIView {
+
+    static let size = CGSize(width: 100, height: 100)
+
+    convenience init(origin: CGPoint) {
+        let rect = CGRect(origin: origin, size: CEEntryDynamicItem.size)
+        self.init(frame: rect)
+    }
+
     override init(frame: CGRect) {
-        super.init(frame: frame)
+        super.init(frame: CGRect(origin: frame.origin, size: CEEntryDynamicItem.size))
         self.backgroundColor = UIColor.brownColor()
+
+        self.transform = CGAffineTransformRotate(self.transform, CGFloat.random()*2*CGFloat(M_PI))
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(Float(arc4random()) / Float(UINT32_MAX))
     }
 }
